914c3f4e6feed98e822d17da888511d48d904e0a
Tiana Johnson <tiana.marie013@gmail.com>
2019-02-01 16:37:51 -0600

    sql added as a file.

--- server/modules/portfolio.router.js
+++ server/modules/portfolio.router.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool');
 
-
+router.get('/', (rec, res) => {
+    
+})
 
 module.exports = router;
